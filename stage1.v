3aeb2102ed3718c2f0830c22eb64d8f3c8cd62ec
Ahmad Urabi <hamod9041@gmail.com>
2023-01-11 14:47:05 +0300

    CSL Works!!

--- stage1.v
+++ stage1.v
@@ -1,8 +1,27 @@
-module f(input [31:0] x,y,z, output reg [31:0] out);
-    always @(x && y && z)
+module csl(input [31:0] in, input [31:0] n, output reg [31:0] out);
+reg [31:0] temp;
+reg [31:0] temp_old;
+integer y;
+integer x;
+always@(*)
+begin
+    temp = in;
+    temp_old = in;
+    for(x = 0; x<n; x++)
     begin
-        out = (x && y) || (!x && z);
+        temp[0] = temp_old[31];
+        for(y=0;y<31;y++)
+        begin
+            temp[y+1] = temp_old[y];
+        end
+        temp_old = temp;
     end
+    out = temp;
+end
+endmodule
+
+module f(input [31:0] x,y,z, output [31:0] out);
+        assign out = (x && y) || (~x && z);
 endmodule
 
 module function_1(a,b,c,d,s,k,out);
@@ -11,25 +30,26 @@ input [31:0]k;
 input [31:0] s;
 output reg [31:0] out;
 wire [31:0] f_out;
-reg [31:0] temp;
+wire [31:0] temp;
+wire [31:0] csl_out, csl_out_2, csl_out_3;
 reg [31:0] temp_2;
 integer y;
+integer x;
 f f_1 (b,c,d,f_out);
-
-always@(a && b && c && d && s && k && f_out)
+assign temp = (a + f_out + k);
+csl cs(temp,s,csl_out);
+always @(csl_out)
 begin
-    if(f_out == 1 || f_out == 0)
-    begin
-    temp = (a + f_out + k);
-    out <= temp;
-    end
+    $display("%b %b", temp, csl_out);
+    out = csl_out;
 end
 endmodule
 
-module stage1(a,b,c,d,x,out_a, out_b, out_c, out_d);
+module stage1(a,b,c,d,x,out_a, out_b, out_c, out_d,clk);
 input [31:0] a,b,c,d;
 input [511:0] x;
-output reg [31:0] out_a, out_b, out_c, out_d;
+input clk;
+output [31:0] out_a, out_b, out_c, out_d;
 wire [31:0] a_1,a_2,a_3,a_4;
 wire [31:0] b_1,b_2,b_3,b_4;
 wire [31:0] c_1,c_2,c_3,c_4;
@@ -52,14 +72,10 @@ function_1 f14(d_3,a_4,b_3,c_3,7,x[447:416],d_4);
 function_1 f15(c_3,d_4,a_4,b_3,11,x[479:448],c_4);
 function_1 f16(b_3,c_4,d_4,a_4,19,x[511:480],b_4);
 
-always@(a && b && c && d)
-begin
-    $display("%b %b %b %b",a_1,b_1,c_1,d_1);
-    out_a = a_4;
-    out_b = b_4;
-    out_c = c_4;
-    out_d = d_4;
-end
+assign out_a = a_4;
+assign out_b = b_4;
+assign out_c = c_4;
+assign out_d = d_4;
 endmodule
 
 module stage_1_tb();
@@ -67,7 +83,8 @@ reg [31:0] a,b,c,d;
 wire [31:0] res_a, res_b, res_c, res_d;
 reg [15:0] x[15:0];
 reg [511:0] M;
-stage1 s1(a,b,c,d,M,res_a,res_b,res_c,res_d);
+reg clk;
+stage1 s1(a,b,c,d,M,res_a,res_b,res_c,res_d,clk);
 initial
 begin
 #1 a = 32'h67453201;
